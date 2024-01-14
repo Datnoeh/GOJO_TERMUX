9a180c4147ee1e5f319b86d0dd7fe3bee4f85fdd
Datnoeh <155913848+Datnoeh@users.noreply.github.com>
2024-01-14 22:58:22 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
-echo"đang setup vui lòng chờ đến khi hoàn tất....."
+echo "Đang setup, vui lòng chờ đến khi hoàn tất....."
 termux-setup-storage
-yes | (update && apt upgrade -y && pkg install wget && wget openjdk-17 && pkg install openjdk && pkg install ant && pkg install dropbear && pkg install openssh && pkg install tsu && sudo netstat -tulpn | grep :22 && pkg install python && pkg install php && pkg install openssl-tool
-
-
-
+yes | (pkg update && pkg upgrade -y && pkg install wget && pkg install openjdk-17 && pkg install ant && pkg install dropbear && pkg install openssh && pkg install tsu && pkg install net-tools && pkg install python && pkg install php && pkg install openssl-tool
 
 # Tiếp tục với các bước cài đặt khác
-wget -O menu.zip  https://datnoeh.github.io/GOJO_TERMUX/menu/menu.zip
+wget -O menu.zip https://datnoeh.github.io/GOJO_TERMUX/menu/menu.zip
 unzip menu.zip
-wget -O setup.py  https://datnoeh.github.io/GOJO_TERMUX/setup.py && python setup.py
+wget -O setup.py https://datnoeh.github.io/GOJO_TERMUX/setup.py && python setup.py
 stty -echo
-trap '' SIGINT)  > /dev/null 2>&1
+trap '' SIGINT) > /dev/null 2>&1
